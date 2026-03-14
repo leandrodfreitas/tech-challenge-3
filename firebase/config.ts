6dffbf06370cebfeae0275e878493dfdfa7ea481
Leandro de Freitas Rodrigues <leandrodfreitas@gmail.com>
2026-03-13 22:44:39 -0300

    Ajuste config.ts

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,17 +1,37 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { FirebaseApp, getApp, getApps, initializeApp } from "firebase/app";
+// @ts-ignore
+import { getAuth, Auth, initializeAuth, getReactNativePersistence } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "tech-challenge-3-9f1ee.firebaseapp.com",
-  projectId: "tech-challenge-3-9f1ee",
-  storageBucket: "tech-challenge-3-9f1ee.firebasestorage.app",
-  messagingSenderId: "855400261531",
-  appId: "1:855400261531:web:c0e93d5da639ca250da33e"
+	apiKey: "AIzaSyAKhRKx641BNGlim9jdqtqbSdILO3GiLbU",
+	authDomain: "tech-challenge-3-9f1ee.firebaseapp.com",
+	projectId: "tech-challenge-3-9f1ee",
+	storageBucket: "tech-challenge-3-9f1ee.firebasestorage.app",
+	messagingSenderId: "855400261531",
+	appId: "1:855400261531:web:c0e93d5da639ca250da33e",
+	measurementId: "G-LNYKPCEQ07"
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+let app: FirebaseApp
+let auth: Auth
 
-export const auth = getAuth(app);
 
-export default app;
+if (!getApps().length) {
+
+	const app = initializeApp(firebaseConfig);
+
+	auth = initializeAuth(app, {
+		persistence: getReactNativePersistence
+	})
+
+} else {
+	app = getApp()
+	auth = getAuth(app)
+}
+
+export { app, auth }
